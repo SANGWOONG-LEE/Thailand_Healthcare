206692ffce4f7b8fa1186582be2bc0f5d57f8298
Young Suk <you@example.com>
2022-03-24 19:18:30 -0400

    gather data

--- scripts/01-gather_data.R
+++ scripts/01-gather_data.R
@@ -3,80 +3,86 @@ library(pdftools)
 library(purrr)
 library(tidyverse)
 library(stringi)
+library(tesseract)
 
-download.file(
-  "https://dhsprogram.com/pubs/pdf/FR37/FR37.pdf", 
-  "1987_thailand_dhs.pdf",
-  mode="wb")
-
-all_content <- pdf_text("2019_Kenya_census.pdf")
-
-# The function is going to take an input of a page
-get_data <- function(i){
-  # i = 467
-  # Just look at the page of interest
-  # Based on Bob Rudis: https://stackoverflow.com/a/47793617
-  just_page_102 <- stri_split_lines(all_content[[102]])[[102]] 
+text <- tesseract::ocr(here::here("inputs/data/figure_6.9.png"), engine = tesseract("eng"))
+cat(text)
   
-  just_page_i <- just_page_i[just_page_i != ""]
+get_data <- function(text){
+  print(length(text))
   
-  # Grab the name of the location
-  area <- just_page_i[3] |> str_squish()
-  area <- str_to_title(area)
+  immune_data <- tibble(raw_text = text)
+  immune_data <- 
+    separate_rows(immune_data, raw_text, sep = "\\n", convert = FALSE)
+  
+  immune_data <- immune_data[-c(1:7),]
+  immune_data <- immune_data[-c(19:20),]
+  #immune_data <- extract(immune_data, raw_text, into=c('v1', 'v2'), '(\\d+)(\\D+)', convert=TRUE)
+  
   
   # Grab the type of table
-  type_of_table <- just_page_i[2] |> str_squish()
+  #gettype_of_table <- just_page_i[2] |> str_squish()
   
   # Get rid of the top matter
   # Manually for now, but could create some rules if needed
-  just_page_i_no_header <- just_page_i[5:length(just_page_i)] 
+  # no_header <- text[7:length(text)]
   
   # Get rid of the bottom matter
   # Manually for now, but could create some rules if needed
-  just_page_i_no_header_no_footer <- just_page_i_no_header[1:62] 
+  # table_header_no_footer <- no_header[1:25] 
   
   # Convert into a tibble
-  demography_data <- tibble(all = just_page_i_no_header_no_footer)
+  # demography_data <- tibble(all = table_header_no_footer)
   
   # Split columns
-  demography_data <-
-    demography_data |>
-    mutate(all = str_squish(all)) |> # Any space more than two spaces is reduced
-    mutate(all = str_replace(all, "10 -14", "10-14")) |> # One specific issue
-    mutate(all = str_replace(all, "Not Stated", "NotStated")) |> # And another
-    separate(col = all,
-             into = c("age", "male", "female", "total", "age_2", "male_2", "female_2", "total_2"),
+  immune_data <-
+    immune_data |>
+    mutate(raw_text = str_squish(raw_text)) |> # Any space more than two spaces is reduced
+    mutate(raw_text = str_replace(raw_text, "Urban-rual residence", "Urban-ruralResidence")) |> # One specific issue
+    mutate(raw_text = str_replace(raw_text, "5 Northeast", "NorthEast")) |> # And another
+    mutate(raw_text = str_replace(raw_text, "N Central", "Central")) |>
+    mutate(raw_text = str_replace(raw_text, "No education", "NoEducation")) |>
+    mutate(raw_text = str_replace(raw_text, "15-1", "15.1")) |>
+    separate(col = raw_text,
+             into = c("Variable", "Health_Record", "Mothers_Report", "Received_Immunization", "Not_Received_Immunization", "Total_Percent", "Number_of_Children"),
              sep = " ", # Works fine because the tables are nicely laid out
              remove = TRUE,
              fill = "right",
              extra = "drop"
     )
   
+  immune_data <- immune_data |> 
+    #select("Health_Record", "Mother's_Report", "Received_Immunization", "Not_Received_Immunization", "Total_Percent", "Number_of_Children") |>
+    mutate_at(vars(Health_Record, Mothers_Report, Received_Immunization, Not_Received_Immunization, Total_Percent, Number_of_Children), ~str_remove_all(., ",")) |>
+    mutate_at(vars(Health_Record, Mothers_Report, Received_Immunization, Not_Received_Immunization, Total_Percent, Number_of_Children), ~str_replace(., "_", "0")) |>
+    mutate_at(vars(Health_Record, Mothers_Report, Received_Immunization, Not_Received_Immunization, Total_Percent, Number_of_Children), ~str_replace(., "-", "0")) |>
+    mutate_at(vars(Health_Record, Mothers_Report, Received_Immunization, Not_Received_Immunization, Total_Percent, Number_of_Children), ~as.integer(.))
+  
   # They are side by side at the moment, need to append to bottom
-  demography_data_long <-
-    rbind(demography_data |> select(age, male, female, total),
-          demography_data |>
-            select(age_2, male_2, female_2, total_2) |>
-            rename(age = age_2, male = male_2, female = female_2, total = total_2)
-    )
+  # demography_data_long <-
+  #   rbind(demography_data |> select(age, male, female, total),
+  #         demography_data |>
+  #           select(age_2, male_2, female_2, total_2) |>
+  #           rename(age = age_2, male = male_2, female = female_2, total = total_2)
+  #   )
   
   # There is one row of NAs, so remove it
-  demography_data_long <- 
-    demography_data_long |> 
-    remove_empty(which = c("rows"))
+  # demography_data_long <- 
+  #   demography_data_long |> 
+  #   remove_empty(which = c("rows"))
   
   # Add the area and the page
-  demography_data_long$area <- area
-  demography_data_long$table <- type_of_table
-  demography_data_long$page <- i
+  # demography_data_long$area <- area
+  # demography_data_long$table <- type_of_table
+  # demography_data_long$page <- i
   
-  rm(just_page_i,
-     i,
-     area,
-     type_of_table,
-     just_page_i_no_header,
-     just_page_i_no_header_no_footer,
-     demography_data)
+  # rm(just_page_i,
+  #    i,
+  #    area,
+  #    type_of_table,
+  #    just_page_i_no_header,
+  #    just_page_i_no_header_no_footer,
+  #    demography_data)
   
-  return(demography_data_long)
+  return(immune_data)
 }
